a094ea775e442100e5acad728bfb4a3d7c84edce
Maxime Ripard <maxime.ripard@free-electrons.com>
2013-10-05 14:53:48 +0200

    ARM: sunxi: Register the A31 reset IP in init_time
    The A31 has a reset IP that maintains a few other IPs in reset by
    default. Among these IPs are the UARTs, and most notably the timers. We
    thus need to register the reset driver before initializing the timers so
    that the reset timer can use the reset framework.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- arch/arm/mach-sunxi/sunxi.c
+++ arch/arm/mach-sunxi/sunxi.c
@@ -240,9 +240,16 @@ static const char * const sun6i_board_dt_compat[] = {
 	NULL,
 };
 
+extern void __init sun6i_reset_init(void);
+static void __init sun6i_timer_init(void)
+{
+	sunxi_timer_init();
+	sun6i_reset_init();
+}
+
 DT_MACHINE_START(SUN6I_DT, "Allwinner sun6i (A31) Family")
 	.init_machine	= sunxi_dt_init,
-	.init_time	= sunxi_timer_init,
+	.init_time	= sun6i_timer_init,
 	.dt_compat	= sun6i_board_dt_compat,
 	.restart	= sun6i_restart,
 MACHINE_END
