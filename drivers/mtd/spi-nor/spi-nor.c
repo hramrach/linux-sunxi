fddba045287b0b8c1e6f0446b352a327f987090e
Michal Suchanek <hramrach@gmail.com>
2016-05-16 22:12:22 +0200

    TEST: mtd: spi-nor: m25p80: allow writing partial pages
    On spi controllers with really short transfer length whole pages cannot
    be written. This is probably useful for testing only.
    
    Signed-off-by: Michal Suchanek <hramrach@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1158,7 +1158,7 @@ static int spi_nor_write(struct mtd_info *mtd, loff_t to, size_t len,
 		ssize_t written;
 
 		page_offset = (to + i) & (nor->page_size - 1);
-		WARN_ONCE(page_offset,
+		dev_warn(nor->dev,
 			  "Writing at offset %zu into a NOR page. Writing partial pages may decrease reliability and increase wear of NOR flash.",
 			  page_offset);
 		/* the size of data remaining on the first page */
@@ -1177,11 +1177,9 @@ static int spi_nor_write(struct mtd_info *mtd, loff_t to, size_t len,
 		*retlen += written;
 		i += written;
 		if (written != page_remain) {
-			dev_err(nor->dev,
+			dev_warn(nor->dev,
 				"While writing %zu bytes written %zd bytes\n",
 				page_remain, written);
-			ret = -EIO;
-			goto write_err;
 		}
 	}
 
